0ba78bbf7797a05afee86cb138f1f0e126fb1f95
Jaspreet Singh <>
2018-01-12 19:17:51 -0500

    shape like inverted L filled with #

--- src/printpattern1/PrintPattern1.java
+++ src/printpattern1/PrintPattern1.java
@@ -17,7 +17,7 @@ public class PrintPattern1 {
     public static void main(String[] args) {
           for(int row=0;row<=7;row++){
           for(int col=0;col<=7;col++){
-             if(row>col){
+             if(row<col){
               System.out.print("# ");
              }
              else
